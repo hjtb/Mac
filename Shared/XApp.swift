2dd4e745965f4e3fe69c23d6a7b477be19aa1268
hjtb <willholtonjtb@gmail.com>
2021-03-16 08:28:25 +0000

    Initial Commit

---
+++ Shared/XApp.swift
@@ -0,0 +1,17 @@
+//
+//  XApp.swift
+//  Shared
+//
+//  Created by user924386 on 3/16/21.
+//
+
+import SwiftUI
+
+@main
+struct XApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
